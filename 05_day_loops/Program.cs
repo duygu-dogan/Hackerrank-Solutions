364a7eda574a96110e34650855f37fc2dab9f6d9
duygu-dogan <duygudogan1509@gmail.com>
2024-01-30 16:07:09 +0300

    day 5 and 6 added

---
+++ 05_day_loops/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _05_day_loops;
+
+class Solution
+{
+    // Given an integer, n, print its first 10 multiples. Each multiple n x i (where 1 <= i <= 10) should be printed on a new line in the form: n x i = result.
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+        for (int i = 1; i <= 10; i++)
+
+        {
+            if (n <= 2 && n >= 20)
+                n *= i;
+            Console.WriteLine(n + " x " + i + " = " + n * i);
+
+        }
+    }
+}
---
+++ 06_day_lets_review/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _06_day_lets_review;
+
+class Solution
+{
+    //     Task
+    // Given a string, S, of length N that is indexed from 0 to N-1, print its even-indexed and odd-indexed characters as 2 space-separated strings on a single line (see the Sample below for more detail).
+
+    // Note: 0 is considered to be an even index.
+    static void Main(string[] args)
+    {
+        int T = Convert.ToInt32(Console.ReadLine());
+        while (T <= 0 || T > 10)
+        {
+            Console.WriteLine("You entered a wrong number. Please enter the number of tests you would like to perform. It must be between 1 and 10.");
+            T = Convert.ToInt32(Console.ReadLine());
+        }
+        List<string> wordList = new List<string>();
+
+        for (int i = 0; i < T; i++)
+        {
+            wordList.Add(Console.ReadLine());
+        }
+
+        foreach (var item in wordList)
+        {
+            List<char> oddLetters = new List<char>();
+            List<char> evenLetters = new List<char>();
+            for (int i = 0; i < item.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    evenLetters.Add(item[i]);
+                }
+                else
+                {
+                    oddLetters.Add(item[i]);
+                }
+            }
+            string evens = new string(evenLetters.ToArray());
+            string odds = new string(oddLetters.ToArray());
+            Console.WriteLine(evens + " " + odds);
+        }
+
+    }
+}
