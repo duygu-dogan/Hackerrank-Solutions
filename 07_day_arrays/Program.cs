16d9d67128516c03ffd68708cbed177a6c4cd62a
duygu-dogan <duygudogan1509@gmail.com>
2024-01-31 01:14:17 +0300

    two solutions for day 7

---
+++ 07_day_arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _07_day_arrays;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+        
+        int[] array = arr.ToArray();
+
+        // Array.Reverse(array);
+
+        // foreach (var item in array)
+        // {
+        //     Console.Write(item + " ");
+        // }
+
+        //or
+        
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+}
