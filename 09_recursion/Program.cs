cf280671b9dba322db7a44416c0778275c685b53
duygu-dogan <duygudogan1509@gmail.com>
2024-02-01 21:26:01 +0300

    day 9 and 10 added

---
+++ 09_recursion/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _09_recursion;
+
+class Result
+{
+
+    /*
+     * Complete the 'factorial' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int factorial(int n)
+    {
+        if (n == 1)
+        {
+            return n;
+        }else{
+            int result = n * factorial(n-1);
+            return result;
+        }   
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int result = Result.factorial(n);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ 10_binary_numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _10_binary_numbers;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+        
+        string newNumS = getBinary(n);
+        var result = newNumS.Split("0").OrderByDescending(x => x.Length);
+       Console.WriteLine(result.First().Length);
+    
+
+    // short version
+    // var b = Convert.ToString(n, 2).Split('0')
+	// 		.OrderByDescending(x => x.Length);
+	// Console.WriteLine(b.First().Length);
+
+    }
+    public static string getBinary(int n)
+    {
+        List<int> newNum = new List<int>();
+        while (n > 0)
+        {
+            int remainder = n % 2;
+            newNum.Add(remainder);
+            n = n / 2;
+        }
+        string newNumS = string.Join( "", newNum);
+        return newNumS;
+    }
+}
