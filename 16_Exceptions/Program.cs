c2e839e7d8433ede301aa7c536166359b2e63328
duygu-dogan <duygudogan1509@gmail.com>
2024-02-08 22:18:47 +0300

    Day 16 solved

---
+++ 16_Exceptions/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _16_Exceptions;
+
+class Program
+{
+    //     Task
+    // Read a string, S, and print its integer value; if S cannot be converted to an integer, print Bad String.
+
+    // Note: You must use the String-to-Integer and exception handling constructs built into your submission language. If you attempt to use loops/conditional statements, you will get a 0 score.
+    static void Main(string[] args)
+    {
+        string S = Console.ReadLine();
+        try
+        {
+            Console.WriteLine(Convert.ToInt32(S));
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Bad String");
+            throw;
+        }
+    }
+}
