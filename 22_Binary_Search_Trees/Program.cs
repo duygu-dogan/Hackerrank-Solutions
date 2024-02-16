3aeea5328a66b553feabb27f05f050c25a6738aa
duygu-dogan <duygudogan1509@gmail.com>
2024-02-16 17:12:40 +0300

    Day 21(Generics) and 22 (Binary Search Tree) solved

---
+++ 22_Binary_Search_Trees/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+class Node{
+    public Node left,right;
+    public int data;
+    public Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class Solution{
+//     Task
+// The height of a binary search tree is the number of edges between the tree's root and its furthest leaf. You are given a pointer, root, pointing to the root of a binary search tree. Complete the getHeight function provided in your editor so that it returns the height of the binary search tree.
+
+	static int getHeight(Node root){
+      //Write your code here
+
+      int leftNode = 0;
+      int rightNode = 0;
+        if(root.left !=null) 
+        {
+            leftNode = getHeight(root.left) + 1;
+        }
+        if(root.right !=null) 
+        {
+            rightNode= getHeight (root.right) +1;
+        }
+      return Math.Max(leftNode, rightNode);
+      
+    }
+
+	static Node insert(Node root, int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    static void Main(String[] args){
+        Node root=null;
+        int T=Int32.Parse(Console.ReadLine());
+        while(T-->0){
+            int data=Int32.Parse(Console.ReadLine());
+            root=insert(root,data);            
+        }
+        int height=getHeight(root);
+        Console.WriteLine(height);
+        
+    }
+}
