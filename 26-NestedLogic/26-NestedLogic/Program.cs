b7deb872d067cccf280cbb635686b64ac7628fbe
Duygu Doğan <duygudogan1509@gmail.com>
2024-06-29 23:58:35 +0300

    updated,- failed tests are passed

--- 26-NestedLogic/26-NestedLogic/Program.cs
+++ 26-NestedLogic/26-NestedLogic/Program.cs
@@ -3,8 +3,8 @@
     static void Main(String[] args)
     {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
-        DateOnly returned = DateOnly.ParseExact(Console.ReadLine(), "d M y");
-        DateOnly expected = DateOnly.ParseExact(Console.ReadLine(), "d M y");
+        DateTime returned = DateConverter(Console.ReadLine());
+        DateTime expected = DateConverter(Console.ReadLine());
 
         if (returned <= expected)
         {
@@ -21,6 +21,21 @@
         else
         {
             Console.WriteLine(15 * (returned.Day - expected.Day));
+        }
+    }
+    static DateTime DateConverter(string date)
+    {
+        string[] dateString = date.Split(' ');
+        if (dateString.Length != 3)
+        {
+            throw new ArgumentException("Invalid date format");
+        }
+        else
+        {
+            int day = int.Parse(dateString[0]);
+            int month = int.Parse(dateString[1]);
+            int year = int.Parse(dateString[2]);
+            return new DateTime(year, month, day);
         }
     }
 }
